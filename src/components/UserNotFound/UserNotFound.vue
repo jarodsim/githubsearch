eb62385213227bbc27b5fb47c0009bbb07237fbb
jarodsim <jarodsim@gmail.com>
2021-04-22 19:58:08 -0300

    feat: adicionando página de usuário não encontrado

---
+++ src/components/UserNotFound/UserNotFound.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="user_not_found_body">
+    <h1>Eu sei que dói :( Mas não consegui encontrar esse usuário :/</h1>
+    <img src="../../assets/404.png" alt="imagem de usuário não encontrado" />
+  </div>
+</template>
+
+<style scoped>
+#user_not_found_body {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+#user_bot_found_body h1 {
+  font-size: 1em;
+}
+
+#user_not_found_body img {
+  width: 50%;
+}
+
+@media screen and (max-width: 600px) {
+  #user_not_found_body img {
+    width: 100%;
+  }
+}
+</style>
